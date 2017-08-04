64215d77106485fe77ccf6292edc6b0589d2b51c
andycanterbury <andrew.canterbury@ingagepartners.com>
2017-08-04 11:45:42 -0400

    Get form from formio,
    and prepopulate with hard coded data

---
+++ crds-formio-poc/Services/FormIoService.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp;
+
+namespace crds_formio_poc.Services
+{
+    public class FormIoService
+    {
+        public string GetFormData(string formName)
+        {
+            var formIoClient = new RestClient("https://crossroads.form.io/");
+            var formRequest = new RestRequest(formName);
+
+            var formData = formIoClient.Execute(formRequest).Content;
+            return formData;
+        }
+    }
+}
---
+++ crds-formio-poc/Services/MpService.cs
@@ -0,0 +1,11 @@
+﻿namespace crds_formio_poc.Services
+{
+    public class MpService
+    {
+        public string GetFormPreFillData(string formData)
+        {
+            var submission = "{data:{profileModuleFirstName:\"Andy\", profileModuleLastName:\"Canterbury\"}}";
+            return submission;
+        }
+    }
+}
